6e7b05d502fc2bc0d16e5ae7d61719ef3e0207fc
sunil <sunil@inapp.com>
2017-10-10 12:03:05 +0530

    upadtes

--- UI/Source/Validify/src/app/utils/http-service/http.object.ts
+++
@@ -1,8 +0,0 @@
-
-import { URLSearchParams } from "@angular/http";
-
-export class HttpObject {
-	public data: URLSearchParams = new URLSearchParams();
-	public global: boolean = true;
-	constructor() { }
-}
