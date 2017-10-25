80446ba5703556794fb1a4579036b127252b4af5
sunil <sunil@inapp.com>
2017-10-25 16:21:28 +0530

    updates

---
+++ UI/Source/Validify/src/app/doi/dashboard/agency.ts
@@ -0,0 +1,13 @@
+export class Agency {
+  public IsBondAgency:boolean;
+  public AgencyLicenseNum:string;
+  public AgencyName:string;
+  public AgencyPhoneNo:string;
+  public Address:string;
+  public City:string;
+  public State:string;
+  public County:string;
+  public ZipCode:string;
+  public AgencyEmail:string;  
+}
+
