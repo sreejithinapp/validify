10e61e67696d1ee27036f223584f4379c47a3bbc
sunil <sunil@inapp.com>
2017-10-25 12:49:05 +0530

    updates

---
+++ UI/Source/Validify/src/app/surety/dashboard/agent.ts
@@ -0,0 +1,9 @@
+export class Agent {
+  public IsBondAgent:boolean;
+  public AgencyLicenseNum:string;
+  public AgentFirstName:string;
+  public AgentLastName:string;
+  public AgentPhoneNo:string;
+  public AgentEmail:string;
+}
+
